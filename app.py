ed80ec86c8eba6c90dd5c0c20bc0f87a42bcf72d
AST-TheCoder <tonmoy.cse5.bu@gmail.com>
2023-03-15 15:27:08 +0600

    Readme updated

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import sklearn
 import xgboost
 from xgboost import XGBRegressor
+import hashlib
 
 pipe = pickle.load(open('pipe.pkl','rb'))
 
@@ -58,13 +59,17 @@ cities = ['Colombo',
 st.title('Cricket Score Predictor')
 
 col1, col2 = st.columns(2)
-
+def hash_string(s):
+    return int(hashlib.sha256(s.encode('utf-8')).hexdigest(), 16)
 with col1:
     batting_team = st.selectbox('Select batting team',sorted(teams))
+    team1=hash_string(batting_team)
 with col2:
     bowling_team = st.selectbox('Select bowling team', sorted(teams))
+    team2=hash_string(bowling_team)
 
 city = st.selectbox('Select city',sorted(cities))
+venue=hash_string(city)
 
 col3,col4,col5 = st.columns(3)
 
@@ -77,15 +82,13 @@ with col5:
 
 last_five = st.number_input('Runs scored in last 5 overs')
 
-st.write(sklearn.__version__)
-
 if st.button('Predict Score'):
     balls_left = 120 - (overs*6)
     wickets_left = 10 -wickets
     crr = current_score/overs
 
     input_df = pd.DataFrame(
-     {'batting_team': [batting_team], 'bowling_team': [bowling_team],'city':city, 'current_score': [current_score],'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five]})
+     {'hashed_batting_team': [team1], 'hashed_bowling_team': [team2],'city':venue, 'current_score': [current_score],'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five]})
     result = pipe.predict(input_df)
     st.header("Predicted Score - " + str(int(result[0])))
 
