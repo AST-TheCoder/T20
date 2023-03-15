cb80855567563b165e9fdc611bb8edc54df18f1d
AST-TheCoder <tonmoy.cse5.bu@gmail.com>
2023-03-15 19:02:31 +0600

    Readme updated

--- app.py
+++ app.py
@@ -85,7 +85,6 @@ if st.button('Predict Score'):
     input_df = pd.DataFrame(
      {'batting_team': [batting_team], 'bowling_team': [bowling_team],'city':[city], 'current_score': [current_score],'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five]})
     
-    st.write(xgboost.__version__)
     result = pipe.predict(input_df)
     st.header("Predicted Score - " + str(int(result[0])))
 
